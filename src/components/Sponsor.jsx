f3ab86508d793299880e9bb58388ef404533818b
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-06-27 20:28:09 +0700

    Update sponsor section logo

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion';
 import mahameru from '../assets/logos/mahameru.png';
 import dicoding from '../assets/logos/dicoding.png';
 import ldp from '../assets/logos/ldp.jpg';
+import mulia from '../assets/logos/mulia.png';
 
 export const Sponsor = () => (
   <section className='py-12 sm:py-24 bg-customDarkBg2 w-full mt-16 mb-16'>
@@ -24,7 +25,7 @@ export const Sponsor = () => (
             </div>
           </div>
           <div className='w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10'>
-            <div className='flex -m-4 justify-center space-x-5'>
+            <div className='-m-4 grid grid-cols-3 gap-4'>
               <div className='w-full sm:w-full p-2 lg:py-5 lg:px-6 flex justify-center items-center rounded-xl bg-white'>
                 <img
                   src={dicoding}
@@ -41,6 +42,9 @@ export const Sponsor = () => (
               </div>
               <div className='w-full sm:w-full p-2 lg:py-5 lg:px-6 flex justify-center items-center rounded-xl bg-white'>
                 <img src={ldp} alt='LDP' className='w-72 lg:w-72' />
+              </div>
+              <div className='w-full sm:w-full p-2 py-8 lg:py-16 lg:px-6 flex justify-center items-center rounded-xl bg-white'>
+                <img src={mulia} alt='mulia' className='w-72 lg:w-72' />
               </div>
             </div>
           </div>
